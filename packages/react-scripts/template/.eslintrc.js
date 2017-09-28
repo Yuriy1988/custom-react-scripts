a128069c911eda8cbd3c13df97c6ebdf0f4c395e
Oleksandr Dziuban <oleksandr_dziuban@epam.com>
2017-09-28 15:36:08 +0300

    New folder structure added

--- packages/react-scripts/template/.eslintrc.js
+++ packages/react-scripts/template/.eslintrc.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const graphqlEnv = process.env['REACT_APP_GRAPHQL'] || '';
 const isGraphqlActivated = graphqlEnv.indexOf('true') !== -1;
 
-// Custom ESlint settings for .graphql files
+// Custom eslint settings for .graphql files
 module.exports = isGraphqlActivated
   ? {
       parser: 'babel-eslint',
