f1f15cf61bb638b63fa38027cfc3840efa9c4187
Oleksandr Dziuban <oleksandr_dziuban@epam.com>
2017-10-04 15:23:20 +0300

    ESLint config updated

--- packages/react-scripts/template/src/features/Home/components/Message/Message.js
+++ packages/react-scripts/template/src/features/Home/components/Message/Message.js
@@ -5,7 +5,7 @@ export const Message = () => {
   return (
     <div className={messageCss.message}>
       <div>
-        <img className={messageCss.logo} src="/logo.jpg" alt="Logo" />
+        <img alt="Logo" className={messageCss.logo} src="/logo.jpg" />
       </div>
       <div className="oc-tooltip oc-tooltip--top oc-tooltip--custom">
         <div className="oc-tooltip__content">
--- packages/react-scripts/template/src/features/Home/containers/HomeView/HomeView.js
+++ packages/react-scripts/template/src/features/Home/containers/HomeView/HomeView.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
 import Header from '../../../../components/Header';
 import Message from '../../components/Message';
-import query from './queries.graphql';
-
-console.log(query);
 
 class HomeView extends Component {
   render() {
