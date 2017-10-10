4d06760b4d594d39b12cfc39ad16493f4ed986a6
Oleksandr Dziuban <oleksandr_dziuban@epam.com>
2017-10-10 12:55:35 +0300

    post-css css-next with plugins added

--- packages/react-scripts/config/custom-react-scripts/options/postcss-options.js
+++ packages/react-scripts/config/custom-react-scripts/options/postcss-options.js
@@ -4,6 +4,9 @@ module.exports = {
   ident: 'postcss',
   plugins: () => [
     require('postcss-flexbugs-fixes'),
+    require('postcss-cssnext'),
+    require('postcss-custom-properties'),
+    require('postcss-import'),
     autoprefixer({
       browsers: ['>1%', 'last 4 versions', 'Firefox ESR', 'not ie < 9'],
       flexbox: 'no-2009',
