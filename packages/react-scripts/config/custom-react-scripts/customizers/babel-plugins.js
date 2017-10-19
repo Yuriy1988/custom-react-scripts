eabea1efe5a33cca43e88b3f0f61460b3323beee
Oleksandr Tymchenko <Oleksandr_Tymchenko@epam.com>
2017-10-19 17:38:35 +0300

    Revert "add Optional Chaining Operator support"
    This reverts commit b0c5e87a764d701158718363898b311871f831b1.

--- packages/react-scripts/config/custom-react-scripts/customizers/babel-plugins.js
+++ packages/react-scripts/config/custom-react-scripts/customizers/babel-plugins.js
@@ -2,7 +2,4 @@ module.exports = {
   DECORATORS: {
     get: () => require.resolve('babel-plugin-transform-decorators-legacy'),
   },
-  OPT_CHAINING: {
-    get: () => require.resolve('babel-plugin-transform-optional-chaining'),
-  },
 };
